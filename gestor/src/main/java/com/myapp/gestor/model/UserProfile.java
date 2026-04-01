fa3203310172b0d9c5c55de6c392e5ea548246d3
Emma <zuluagamoraemmanuel@gmail.com>
2026-03-31 19:23:51 -0500

    Refactor: I added AccesLevelProtected to NoArgsConstructor notation to prevent future errors

--- gestor/src/main/java/com/myapp/gestor/model/UserProfile.java
+++ gestor/src/main/java/com/myapp/gestor/model/UserProfile.java
@@ -14,6 +14,7 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,7 +22,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Builder
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Setter
 @Getter
