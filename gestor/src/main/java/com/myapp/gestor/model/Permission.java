b504115db4993c0e556ac34b09f1f62717f60b38
Emma <zuluagamoraemmanuel@gmail.com>
2026-04-10 22:20:14 -0500

    Refactor: I added variables that SpringSecurity needed

---
+++ gestor/src/main/java/com/myapp/gestor/model/Permission.java
@@ -0,0 +1,29 @@
+package com.myapp.gestor.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "permissions")
+public class Permission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, updatable = false, unique = true)
+    private String name;
+}
