201dd5ba905ce9ad4dd25aba0e6e5011a50271b1
Emma <zuluagamoraemmanuel@gmail.com>
2026-04-19 12:04:54 -0500

    Refactor/item (#14)
    * Refactor: I added a new field
    
    * Refactor: new fields added in entity
    
    * Refactor: gitgnore updated

--- gestor/src/main/java/com/myapp/gestor/model/Item.java
+++ gestor/src/main/java/com/myapp/gestor/model/Item.java
@@ -36,7 +36,9 @@ public class Item {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "Item's description is required")
+    @NotBlank(message = "The product must have a name")
+    private String name;
+
     private String description;
 
     @Positive
@@ -44,7 +46,11 @@ public class Item {
     private BigDecimal price;
 
     @Enumerated(EnumType.STRING)
-    private ItemState state;
+    @Builder.Default
+    private ItemState state = ItemState.ABANDONED;
+
+    @Enumerated(EnumType.STRING)
+    private CategoryEnum category;
 
     @URL(message = "Please provide an url")
     @Column(length = 1024)
@@ -54,7 +60,8 @@ public class Item {
     private Integer needLevel;
 
     @Column(name = "created_at", updatable = false)
-    private LocalDateTime createdAt;
+    @Builder.Default
+    private LocalDateTime createdAt = LocalDateTime.now();
 
     @ManyToOne
     @JoinColumn(name = "user_profile_id", referencedColumnName = "id")
